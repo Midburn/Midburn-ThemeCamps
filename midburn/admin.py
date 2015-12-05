ab55e6eb6cf2c64774415eaf3d9c0bc1baf96f58
DMTintner <dmtintner@gmail.com>
2015-12-05 23:30:56 +0200

    - moved static file dir

--- midburn/admin.py
+++ midburn/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from midburn.models import *
+
+admin.site.register(Camp)
+admin.site.register(CampMember)
+admin.site.register(CampLocation)
+admin.site.register(CampSafety)
+admin.site.register(Workshop)
