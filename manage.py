b9ca23bb77d5a5f5ef2d105349cfc1af93189e24
DMTintner <dmtintner@gmail.com>
2015-12-05 23:34:53 +0200

    - fixed settings module name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "midburn.settings.dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "midburn.config.dev")
 
     from django.core.management import execute_from_command_line
 
